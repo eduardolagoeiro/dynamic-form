0cfe0935c8101c27d463e8ae6ad84e6de21c89fa
Eduardo Lagoeiro <eduardo.lagoeiro@contasimples.com>
2023-01-21 19:11:15 -0300

    skeleton

---
+++ src/routes/build-form/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { Stepper, Step } from '@skeletonlabs/skeleton'
+	import { writable, type Writable } from 'svelte/store'
+
+	const active: Writable<number> = writable(0)
+</script>
+
+<div class="bg-pink-400">Page</div>
+<Stepper
+	{active}
+	length={2}
+	on:complete={() => {
+		console.log('stepper')
+	}}
+>
+	<Step index={0}>
+		<svelte:fragment slot="header">(header)</svelte:fragment>
+		(content)
+	</Step>
+	<Step index={1} locked={true}>(content)</Step>
+</Stepper>
