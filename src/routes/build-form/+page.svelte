26209ab4a41eacbdf9022ec13a8d1d5a8074e7eb
Eduardo Lagoeiro <eduardo.lagoeiro@contasimples.com>
2023-01-22 15:33:44 -0300

    multiple forms

--- src/routes/build-form/+page.svelte
+++ src/routes/build-form/+page.svelte
@@ -1,22 +1,21 @@
 <script lang="ts">
-	import { Stepper, Step } from '@skeletonlabs/skeleton'
+	import { Stepper, Step, RadioGroup, RadioItem } from '@skeletonlabs/skeleton'
 	import { writable, type Writable } from 'svelte/store'
 	import { slide } from 'svelte/transition'
-	import {
-		expression,
-		form,
-		nameMapper,
-		valueMapper,
-		type Conditional,
-		type JSONForm
-	} from './form'
+	import { forms, type Conditional, type JSONForm } from './form'
 	import Select from './select.svelte'
 	import SelectFallback from './select_fallback.svelte'
 	import * as math from 'mathjs'
 
 	const active: Writable<number> = writable(0)
 
-	let values: Record<string, any> = {}
+	let valuesMap: Record<string, Record<string, any>> = forms.reduce<Record<string, any>>(
+		(acc, curr) => {
+			acc[curr.name] = {}
+			return acc
+		},
+		{}
+	)
 
 	function processCondition(conditional: Conditional, value: any) {
 		if (conditional.conditionExpression === 'eq') {
@@ -102,15 +101,33 @@
 		return context
 	}
 
-	$: context = getContext(values, nameMapper, valueMapper)
+	const formIndex: Writable<number> = writable(1)
 
-	$: result = calc(expression, context)
+	formIndex.subscribe(() => {
+		active.set(0)
+	})
+
+	$: form = forms[$formIndex]
+
+	$: values = valuesMap[form.name]
+
+	$: context = getContext(values, form.nameMapper, form.valueMapper)
+
+	$: result = calc(form.expression, context)
 </script>
+
+<div class="p-5">
+	<RadioGroup selected={formIndex}>
+		{#each forms as form, i}
+			<RadioItem value={i}>{form.name}</RadioItem>
+		{/each}
+	</RadioGroup>
+</div>
 
 <div class="flex gap-10 p-5">
 	<div class="flex-1">
 		<h2>Calculation:</h2>
-		<pre>{expression} = {result}</pre>
+		<pre>{form.expression} = {result}</pre>
 
 		<h2 class="pt-5">Context:</h2>
 		<pre>{JSON.stringify(context, null, 2)}</pre>
@@ -122,92 +139,91 @@
 		<pre>{JSON.stringify(form, null, 2)}</pre>
 	</div>
 	<div class="flex-1 p-5">
-		<Stepper
-			{active}
-			buttonTextBack="voltar"
-			buttonClassesBack="btn btn-filled-surface"
-			buttonTextNext="próximo"
-			buttonClassesNext="btn btn-filled-surface"
-			length={form.length}
-			on:complete={() => {
-				console.log('stepper')
-			}}
-		>
-			{#each form as step, i}
-				<Step
-					index={i}
-					locked={!!step.inputs.find((inputForm) => {
-						const { key, optional, multipleOptions, type } = inputForm
-						if (!show(inputForm, values)) return false
+		{#key form.steps.length}
+			<Stepper
+				{active}
+				buttonTextBack="voltar"
+				buttonClassesBack="btn btn-filled-surface"
+				buttonTextNext="próximo"
+				buttonClassesNext="btn btn-filled-surface"
+				length={form.steps.length}
+			>
+				{#each form.steps as step, i}
+					<Step
+						index={i}
+						locked={!!step.inputs.find((inputForm) => {
+							const { key, optional, multipleOptions, type } = inputForm
+							if (!show(inputForm, values)) return false
 
-						if (optional) return false
-						if (type === 'number') {
-							if (inputForm.min !== undefined && values[key] < inputForm.min) {
-								return true
+							if (optional) return false
+							if (type === 'number') {
+								if (inputForm.min !== undefined && values[key] < inputForm.min) {
+									return true
+								}
+								if (inputForm.max !== undefined && values[key] > inputForm.max) {
+									return true
+								}
 							}
-							if (inputForm.max !== undefined && values[key] > inputForm.max) {
-								return true
+							const value = values[key]
+							if (multipleOptions && Array.isArray(value)) {
+								return value.length === 0
 							}
-						}
-						const value = values[key]
-						if (multipleOptions && Array.isArray(value)) {
-							return value.length === 0
-						}
-						return [undefined, ''].includes(values[key])
-					})}
-				>
-					<svelte:fragment slot="header">{step.title}</svelte:fragment>
-					{#each step.inputs as input}
-						{#if show(input, values)}
-							{#if input.type === 'number'}
-								<label for={input.key}>
-									<span>{input.label}</span>
-									<input
-										type="number"
-										id={input.key}
-										bind:value={values[input.key]}
-										min={input.min}
-										max={input.max}
-									/>
-									{#if input.min !== undefined && values[input.key] < input.min}
-										<p transition:slide>
-											Valor não pode ser menor que {input.min}
-										</p>
-									{:else if input.max !== undefined && values[input.key] > input.max}
-										<p transition:slide>
-											Valor não pode ser maior que {input.max}
-										</p>
-									{/if}
-								</label>
-							{:else if input.type === 'select'}
-								<label for={input.key}>
-									{#if input.options}
-										{#if input.multipleOptions}
-											<Select
-												label={input.label}
-												bind:value={values[input.key]}
-												multipleOptions={input.multipleOptions}
-												list={input.options}
-												optionalLabel={input.optional?.label}
-											/>
-										{:else}
-											<SelectFallback
-												label={input.label}
-												bind:value={values[input.key]}
-												list={input.options}
-												noneFallback={input.noneFallback}
-												optionalLabel={input.optional?.label}
-											/>
+							return [undefined, ''].includes(values[key])
+						})}
+					>
+						<svelte:fragment slot="header">{step.title}</svelte:fragment>
+						{#each step.inputs as input}
+							{#if show(input, values)}
+								{#if input.type === 'number'}
+									<label for={input.key}>
+										<span>{input.label}</span>
+										<input
+											type="number"
+											id={input.key}
+											bind:value={values[input.key]}
+											min={input.min}
+											max={input.max}
+										/>
+										{#if input.min !== undefined && values[input.key] < input.min}
+											<p transition:slide>
+												Valor não pode ser menor que {input.min}
+											</p>
+										{:else if input.max !== undefined && values[input.key] > input.max}
+											<p transition:slide>
+												Valor não pode ser maior que {input.max}
+											</p>
 										{/if}
-									{/if}
-								</label>
-							{:else}
-								{input.type}
+									</label>
+								{:else if input.type === 'select'}
+									<label for={input.key}>
+										{#if input.options}
+											{#if input.multipleOptions}
+												<Select
+													label={input.label}
+													bind:value={values[input.key]}
+													multipleOptions={input.multipleOptions}
+													list={input.options}
+													optionalLabel={input.optional?.label}
+												/>
+											{:else}
+												<SelectFallback
+													label={input.label}
+													bind:value={values[input.key]}
+													list={input.options}
+													noneFallback={input.noneFallback}
+													optionalLabel={input.optional?.label}
+												/>
+											{/if}
+										{/if}
+									</label>
+								{:else}
+									{input.type}
+								{/if}
 							{/if}
-						{/if}
-					{/each}
-				</Step>
-			{/each}
-		</Stepper>
+						{/each}
+					</Step>
+				{/each}
+			</Stepper>
+		{/key}
 	</div>
 </div>
--- src/routes/build-form/form.ts
+++ src/routes/build-form/form.ts
@@ -37,352 +37,392 @@ export interface JSONFormStep {
 
 export type JSONFormSteps = JSONFormStep[]
 
-export const form: JSONFormSteps = [
-	{
-		title: 'Moradia',
-		inputs: [
-			{
-				key: 'peopleNumber',
-				type: 'number',
-				label: 'Há quantas pessoas (incluindo você) morando em sua residência?',
-				min: 1,
-				max: 10
-			}
-		]
+export interface ICalculator {
+	name: string
+	expression: string
+	nameMapper: Record<string, string>
+	valueMapper: Record<string, Record<string, number>>
+	steps: JSONFormSteps
+}
+
+export const form1: ICalculator = {
+	name: 'b2c',
+	expression: '(P * 5) + (GV * GF) + ((F_G + F_E + F_D + F_CNG) * FF)',
+	nameMapper: {
+		peopleNumber: 'P',
+		gasValue: 'GV',
+		gasType: 'GF',
+		fuelGasolineValue: 'F_G',
+		fuelEthanolValue: 'F_E',
+		fuelDieselValue: 'F_D',
+		fuelCNGValue: 'F_CNG',
+		transportType: 'FF'
 	},
-	{
-		title: 'Gás',
-		inputs: [
-			{
-				key: 'gasType',
-				type: 'select',
-				label: 'Na sua residência, utiliza-se botijão (GLP) ou Gás encanado?',
-				options: [
-					{
-						value: 'glp',
-						label: 'Botijão (GLP)'
-					},
-					{
-						value: 'encanado',
-						label: 'Gás encanado'
-					},
-					{
-						value: 'semgas',
-						label: 'Nenhum'
-					}
-				]
-			},
-			{
-				skipWhen: [
-					{
-						key: 'gasType',
-						conditionExpression: 'eq',
-						value: 'semgas'
-					}
-				],
-				key: 'gasValue',
-				type: 'select',
-				label: 'Em média, quanto você gasta com gás por mês?',
-				options: [
-					{ value: 'nada', label: 'Nada (R$0,00)' },
-					{ value: 'muito_pouco', label: 'Muito pouco (R$ 21,00)' },
-					{ value: 'pouco', label: 'Pouco (R$ 35,00)' },
-					{ value: 'moderadamente', label: 'Moderadamente (R$ 63,00)' },
-					{ value: 'bastante', label: 'Bastante (R$ 137,00)' }
-				],
-				noneFallback: {
-					label: 'Sabe o valor exato?',
-					type: 'number'
+	valueMapper: {
+		gasType: {
+			glp: 1.2,
+			encanado: 1.5,
+			semgas: 0
+		},
+		gasValue: {
+			nada: 0,
+			muito_pouco: 21,
+			pouco: 35,
+			moderadamente: 63,
+			bastante: 137
+		},
+		transportType: {
+			car: 1.8,
+			bike: 1.7
+		}
+	},
+	steps: [
+		{
+			title: 'Moradia',
+			inputs: [
+				{
+					key: 'peopleNumber',
+					type: 'number',
+					label: 'Há quantas pessoas (incluindo você) morando em sua residência?',
+					min: 1,
+					max: 10
 				}
-			}
-		]
-	},
-	{
-		title: 'Transporte individual',
-		inputs: [
-			{
-				key: 'transportType',
-				type: 'select',
-				label: 'Quais meios de transporte você costuma utilizar no seu dia a dia?',
-				multipleOptions: true,
-				options: [
-					{
-						value: 'car',
-						label: 'Carro'
-					},
-					{
-						value: 'bike',
-						label: 'Bicicleta'
-					},
-					{
-						value: 'motorcycle',
-						label: 'Motocicleta'
-					},
-					{
-						value: 'walk',
-						label: 'Ando a pé'
-					}
-				]
-			},
-			{
-				key: 'fuelGasolineValue',
-				type: 'number',
-				label: 'Em média, quanto você gasta com gasolina por mês?',
-				min: 0,
-				showWhen: [
-					{
-						key: 'transportType',
-						conditionExpression: 'contain',
-						value: 'car'
-					}
-				]
-			},
-			{
-				key: 'fuelEthanolValue',
-				type: 'number',
-				label: 'Em média, quanto você gasta com etanol por mês?',
-				min: 0,
-				showWhen: [
-					{
-						key: 'transportType',
-						conditionExpression: 'contain',
-						value: 'car'
-					}
-				]
-			},
-			{
-				key: 'fuelDieselValue',
-				type: 'number',
-				label: 'Em média, quanto você gasta com diesel por mês?',
-				min: 0,
-				showWhen: [
-					{
-						key: 'transportType',
-						conditionExpression: 'contain',
-						value: 'car'
-					}
-				]
-			},
-			{
-				key: 'fuelCNGValue',
-				type: 'number',
-				label: 'Em média, quanto você gasta com GNV por mês?',
-				min: 0,
-				showWhen: [
-					{
-						key: 'transportType',
-						conditionExpression: 'contain',
-						value: 'car'
-					}
-				]
-			}
-		]
-	},
-	{
-		title: 'Transporte público',
-		inputs: [
-			{
-				key: 'publicTransportType',
-				type: 'select',
-				label: 'Quais meios de transporte público você utiliza?',
-				multipleOptions: true,
-				optional: {
-					label: 'Não utilizo transporte público ou nenhuma das opções acima'
+			]
+		},
+		{
+			title: 'Gás',
+			inputs: [
+				{
+					key: 'gasType',
+					type: 'select',
+					label: 'Na sua residência, utiliza-se botijão (GLP) ou Gás encanado?',
+					options: [
+						{
+							value: 'glp',
+							label: 'Botijão (GLP)'
+						},
+						{
+							value: 'encanado',
+							label: 'Gás encanado'
+						},
+						{
+							value: 'semgas',
+							label: 'Nenhum'
+						}
+					]
 				},
-				options: [
-					{
-						label: 'Metrô/Trem',
-						value: 'subway'
-					},
-					{
-						label: 'Ônibus',
-						value: 'bus'
+				{
+					skipWhen: [
+						{
+							key: 'gasType',
+							conditionExpression: 'eq',
+							value: 'semgas'
+						}
+					],
+					key: 'gasValue',
+					type: 'select',
+					label: 'Em média, quanto você gasta com gás por mês?',
+					options: [
+						{ value: 'nada', label: 'Nada (R$0,00)' },
+						{ value: 'muito_pouco', label: 'Muito pouco (R$ 21,00)' },
+						{ value: 'pouco', label: 'Pouco (R$ 35,00)' },
+						{ value: 'moderadamente', label: 'Moderadamente (R$ 63,00)' },
+						{ value: 'bastante', label: 'Bastante (R$ 137,00)' }
+					],
+					noneFallback: {
+						label: 'Sabe o valor exato?',
+						type: 'number'
 					}
-				]
-			}
-		]
-	},
-	{
-		title: 'Transporte Aéreo',
-		inputs: [
-			{
-				key: 'shortFlightTransportValue',
-				type: 'number',
-				label: 'Quantos vôos de até três horas você realizou no ano de 2022?',
-				min: 0
-			},
-			{
-				key: 'longFlightTransportValue',
-				type: 'number',
-				label: 'Quantos vôos com mais de três horas você realizou no ano de 2022?',
-				min: 0
-			}
-		]
-	},
-	{
-		title: 'Hábitos alimentares',
-		inputs: [
-			{
-				key: 'dailyCaloriesValue',
-				type: 'select',
-				label: 'Em média, quanto você come por dia?',
-				options: [
-					{
-						label: 'Eu como pouco (~1051 kcal)',
-						value: 'pouco'
+				}
+			]
+		},
+		{
+			title: 'Transporte individual',
+			inputs: [
+				{
+					key: 'transportType',
+					type: 'select',
+					label: 'Quais meios de transporte você costuma utilizar no seu dia a dia?',
+					multipleOptions: true,
+					options: [
+						{
+							value: 'car',
+							label: 'Carro'
+						},
+						{
+							value: 'bike',
+							label: 'Bicicleta'
+						},
+						{
+							value: 'motorcycle',
+							label: 'Motocicleta'
+						},
+						{
+							value: 'walk',
+							label: 'Ando a pé'
+						}
+					]
+				},
+				{
+					key: 'fuelGasolineValue',
+					type: 'number',
+					label: 'Em média, quanto você gasta com gasolina por mês?',
+					min: 0,
+					showWhen: [
+						{
+							key: 'transportType',
+							conditionExpression: 'contain',
+							value: 'car'
+						}
+					]
+				},
+				{
+					key: 'fuelEthanolValue',
+					type: 'number',
+					label: 'Em média, quanto você gasta com etanol por mês?',
+					min: 0,
+					showWhen: [
+						{
+							key: 'transportType',
+							conditionExpression: 'contain',
+							value: 'car'
+						}
+					]
+				},
+				{
+					key: 'fuelDieselValue',
+					type: 'number',
+					label: 'Em média, quanto você gasta com diesel por mês?',
+					min: 0,
+					showWhen: [
+						{
+							key: 'transportType',
+							conditionExpression: 'contain',
+							value: 'car'
+						}
+					]
+				},
+				{
+					key: 'fuelCNGValue',
+					type: 'number',
+					label: 'Em média, quanto você gasta com GNV por mês?',
+					min: 0,
+					showWhen: [
+						{
+							key: 'transportType',
+							conditionExpression: 'contain',
+							value: 'car'
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Transporte público',
+			inputs: [
+				{
+					key: 'publicTransportType',
+					type: 'select',
+					label: 'Quais meios de transporte público você utiliza?',
+					multipleOptions: true,
+					optional: {
+						label: 'Não utilizo transporte público ou nenhuma das opções acima'
 					},
-					{
-						label: 'Moderadamente (~1577 kcal)',
-						value: 'moderadamente'
-					},
-					{
-						label: 'Bastante (~2365 kcal)',
-						value: 'bastante'
-					},
-					{
-						label: 'Muito!!! (~3154 kcal)',
-						value: 'muito'
+					options: [
+						{
+							label: 'Metrô/Trem',
+							value: 'subway'
+						},
+						{
+							label: 'Ônibus',
+							value: 'bus'
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Transporte Aéreo',
+			inputs: [
+				{
+					key: 'shortFlightTransportValue',
+					type: 'number',
+					label: 'Quantos vôos de até três horas você realizou no ano de 2022?',
+					min: 0
+				},
+				{
+					key: 'longFlightTransportValue',
+					type: 'number',
+					label: 'Quantos vôos com mais de três horas você realizou no ano de 2022?',
+					min: 0
+				}
+			]
+		},
+		{
+			title: 'Hábitos alimentares',
+			inputs: [
+				{
+					key: 'dailyCaloriesValue',
+					type: 'select',
+					label: 'Em média, quanto você come por dia?',
+					options: [
+						{
+							label: 'Eu como pouco (~1051 kcal)',
+							value: 'pouco'
+						},
+						{
+							label: 'Moderadamente (~1577 kcal)',
+							value: 'moderadamente'
+						},
+						{
+							label: 'Bastante (~2365 kcal)',
+							value: 'bastante'
+						},
+						{
+							label: 'Muito!!! (~3154 kcal)',
+							value: 'muito'
+						}
+					],
+					noneFallback: {
+						label: 'Sabe quantas calorias (kcal)?',
+						type: 'number'
 					}
-				],
-				noneFallback: {
-					label: 'Sabe quantas calorias (kcal)?',
+				}
+			]
+		},
+		{
+			title: 'Hábitos alimentares',
+			inputs: [
+				{
+					key: 'foodHabitsType',
+					type: 'select',
+					label: 'Como você classificaria sua dieta?',
+					options: [
+						{
+							label: 'Alto consumo de carnes',
+							value: 'highly-meat'
+						},
+						{
+							label: 'Consumo de carnes mediano',
+							value: 'some-meat'
+						},
+						{
+							label: 'Flexitariana (carne no máx 3x por semana)',
+							value: 'few-meat'
+						},
+						{
+							label: 'Pesco-vegetariana',
+							value: 'fish-veg'
+						},
+						{
+							label: 'Ovo-lacto-vegetariana',
+							value: 'egg-veg'
+						},
+						{
+							label: 'Vegetariana estrita/vegana',
+							value: 'veg'
+						}
+					]
+				}
+			]
+		},
+		{
+			title: 'Hábitos de consumo',
+			description:
+				'Quantas peças de roupa você comprou em 2022, em cada uma das categorias abaixo?',
+			inputs: [
+				{
+					key: 'clothConsumption1',
+					type: 'number',
+					label: 'Ternos, jaquetas, vestidos, casacos e camisas',
+					min: 0
+				},
+				{
+					key: 'clothConsumption2',
+					type: 'number',
+					label:
+						'Meias, luvas, roupas de banho, roupas íntimas, pijamas, lenços, gravatas e cachecóis',
+					min: 0
+				},
+				{
+					key: 'clothConsumption3',
+					type: 'number',
+					label: 'Camisetas, calças, shorts, saias e roupas de treino',
+					min: 0
+				}
+			]
+		},
+		{
+			title: 'Hábitos de consumo',
+			description:
+				'Quantos gadgets/eletrônicos você comprou em 2022, em cada uma das categorias abaixo?',
+			inputs: [
+				{
+					key: 'eletronicConsumption1',
+					type: 'number',
+					label: 'Tablets, homedots e echodots',
+					min: 0
+				},
+				{
+					key: 'eletronicConsumption2',
+					type: 'number',
+					label: 'Notebooks',
+					min: 0
+				},
+				{
+					key: 'eletronicConsumption3',
+					type: 'number',
+					label: 'Computadores desktop, monitores e TV´s',
+					min: 0
+				}
+			]
+		}
+	]
+}
+
+export const form2: ICalculator = {
+	name: 'numeros',
+	expression: 'PEOPLE * (CALORIES / 100) * (TRASH * 1.5)',
+	nameMapper: {
+		peopleInYourHouse: 'PEOPLE',
+		calories: 'CALORIES',
+		trash: 'TRASH'
+	},
+	valueMapper: {},
+	steps: [
+		{
+			title: 'Moradia',
+			inputs: [
+				{
+					key: 'peopleInYourHouse',
+					min: 1,
+					label: 'Quantas pessoas moram na sua casa',
 					type: 'number'
 				}
-			}
-		]
-	},
-	{
-		title: 'Hábitos alimentares',
-		inputs: [
-			{
-				key: 'foodHabitsType',
-				type: 'select',
-				label: 'Como você classificaria sua dieta?',
-				options: [
-					{
-						label: 'Alto consumo de carnes',
-						value: 'highly-meat'
-					},
-					{
-						label: 'Consumo de carnes mediano',
-						value: 'some-meat'
-					},
-					{
-						label: 'Flexitariana (carne no máx 3x por semana)',
-						value: 'few-meat'
-					},
-					{
-						label: 'Pesco-vegetariana',
-						value: 'fish-veg'
-					},
-					{
-						label: 'Ovo-lacto-vegetariana',
-						value: 'egg-veg'
-					},
-					{
-						label: 'Vegetariana estrita/vegana',
-						value: 'veg'
-					}
-				]
-			}
-		]
-	},
-	{
-		title: 'Hábitos de consumo',
-		description: 'Quantas peças de roupa você comprou em 2022, em cada uma das categorias abaixo?',
-		inputs: [
-			{
-				key: 'clothConsumption1',
-				type: 'number',
-				label: 'Ternos, jaquetas, vestidos, casacos e camisas',
-				min: 0
-			},
-			{
-				key: 'clothConsumption2',
-				type: 'number',
-				label:
-					'Meias, luvas, roupas de banho, roupas íntimas, pijamas, lenços, gravatas e cachecóis',
-				min: 0
-			},
-			{
-				key: 'clothConsumption3',
-				type: 'number',
-				label: 'Camisetas, calças, shorts, saias e roupas de treino',
-				min: 0
-			}
-		]
-	},
-	{
-		title: 'Hábitos de consumo',
-		description:
-			'Quantos gadgets/eletrônicos você comprou em 2022, em cada uma das categorias abaixo?',
-		inputs: [
-			{
-				key: 'eletronicConsumption1',
-				type: 'number',
-				label: 'Tablets, homedots e echodots',
-				min: 0
-			},
-			{
-				key: 'eletronicConsumption2',
-				type: 'number',
-				label: 'Notebooks',
-				min: 0
-			},
-			{
-				key: 'eletronicConsumption3',
-				type: 'number',
-				label: 'Computadores desktop, monitores e TV´s',
-				min: 0
-			}
-		]
-	}
-]
-
-export const nameMapper: Record<string, string> = {
-	peopleNumber: 'P',
-	gasValue: 'GV',
-	gasType: 'GF',
-	fuelGasolineValue: 'F_G',
-	fuelEthanolValue: 'F_E',
-	fuelDieselValue: 'F_D',
-	fuelCNGValue: 'F_CNG',
-	transportType: 'FF'
+			]
+		},
+		{
+			title: 'Comida',
+			inputs: [
+				{
+					key: 'calories',
+					label: 'Em média, quantas calorias cada pessoa come na sua casa?',
+					type: 'number'
+				}
+			]
+		},
+		{
+			title: 'Lixo',
+			inputs: [
+				{
+					key: 'trash',
+					min: 0,
+					label: 'Quantos kgs de lixo sua família produz por dia?',
+					type: 'number'
+				}
+			]
+		}
+	]
 }
 
-export const valueMapper: Record<string, Record<string, number>> = {
-	gasType: {
-		glp: 1.2,
-		encanado: 1.5,
-		semgas: 0
-	},
-	gasValue: {
-		nada: 0,
-		muito_pouco: 21,
-		pouco: 35,
-		moderadamente: 63,
-		bastante: 137
-	},
-	transportType: {
-		car: 1.8,
-		bike: 1.7
-	}
-}
-
-export const expression = '(P * 5) + (GV * GF) + ((F_G + F_E + F_D + F_CNG) * FF)'
-// key: 'transportType',
-// key: 'fuelGasolineValue',
-// key: 'fuelEthanolValue',
-// key: 'fuelDieselValue',
-// key: 'fuelCNGValue',
-// key: 'publicTransportType',
-// key: 'shortFlightTransportValue',
-// key: 'longFlightTransportValue',
-// key: 'dailyCaloriesValue',
-// key: 'foodHabitsType',
-// key: 'clothConsumption1',
-// key: 'clothConsumption2',
-// key: 'clothConsumption3',
-// key: 'eletronicConsumption1',
-// key: 'eletronicConsumption2',
-// key: 'eletronicConsumption3',
+export const forms = [form1, form2]
